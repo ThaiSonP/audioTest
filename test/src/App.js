55087409df6cb08c23e23a165983cfc68f782d36
ThaiSonP <tysonpan@pursuit.org>
2019-04-03 17:28:56 -0400

    it works

---
+++ test/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Test from './test'
+
+import './App.css';
+
+class App extends Component {
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          testing things
+          <Test/>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ test/src/test.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { ReactMic } from 'react-mic';
+
+export class Test extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      record: false,
+      blob:null
+    }
+  }
+
+  startRecording = () => {
+    this.setState({
+      record: true
+    });
+  }
+
+  stopRecording = () => {
+    this.setState({
+      record: false
+    });
+  }
+
+  onData(recordedBlob) {
+    console.log('chunk of real-time data is: ', recordedBlob);
+  }
+
+  onStop= async(recordedBlob)=> {
+    await console.log('recordedBlob is: ', recordedBlob);
+    await this.setState({
+      blob:recordedBlob.blobURL
+    })
+  }
+
+  resetBlob=()=>{
+    this.setState({
+      blob:null
+    })
+  }
+
+  render() {
+    console.log(this.state.blob)
+    return (
+      <div>
+        <ReactMic
+          record={this.state.record}
+          className="sound-wave"
+          onStop={this.onStop}
+          onData={this.onData}
+          strokeColor="#000000"
+          backgroundColor="#FF4081" /><br/>
+        <button onClick={this.startRecording} type="button">Start</button>
+        <button onClick={this.stopRecording} type="button">Stop</button>
+        <button onClick={this.resetBlob}>delete</button><br/>
+        <audio ref="audio_tag" src={this.state.blob} controls />
+      </div>
+    );
+  }
+}
+
+export default Test;
